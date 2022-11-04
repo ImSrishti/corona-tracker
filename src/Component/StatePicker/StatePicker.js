cb327bd7fd1f2e01d9c48b29b98f3ba900ad7098
Srishti Khobragade <ImSrishti>
2022-11-04 17:35:25 +0530

    fixed issues

--- src/Component/StatePicker/StatePicker.js
+++ src/Component/StatePicker/StatePicker.js
@@ -1,26 +1,33 @@
-import React,{useState,useEffect} from 'react'
-import {NativeSelect,FormControl} from '@material-ui/core';
+import React, { useState, useEffect } from 'react';
+import { NativeSelect, FormControl } from '@material-ui/core';
 import styles from './StatePicker.module.css';
-import {fetchStates} from '../../api';
+import { fetchStates } from '../../api';
 
-const StatePicker= ({handleCountryChange}) =>{
+const StatePicker = ({ handleCountryChange }) => {
+  const [fetchedStates, setFetchedStates] = useState([]);
 
-    const [fetchedStates,setFetchedStates] = useState([])
-    useEffect(()=>{
-        const fetchAPI = async () =>{
-            setFetchedStates(await fetchStates());
-        }
-        fetchAPI();
-    },[fetchedStates]);
+  useEffect(() => {
+    const fetchAPI = async () => {
+      setFetchedStates(await fetchStates());
+    };
+    fetchAPI();
+  }, []);
 
-    return (
-        <FormControl className={styles.formControl}>
-            <NativeSelect defaultValue="" onChange={(e)=>handleCountryChange(e.target.value)}>
-                <option value="Global">global</option>
-                {fetchedStates.map((country,i)=><option key={i} value={country}>{country}</option>)}
-            </NativeSelect>
-        </FormControl>
-    )
-}
+  return (
+    <FormControl className={styles.formControl}>
+      <NativeSelect
+        defaultValue=""
+        onChange={(e) => handleCountryChange(e.target.value)}
+      >
+        <option value="Global">global</option>
+        {fetchedStates.map((country, i) => (
+          <option key={i} value={country}>
+            {country}
+          </option>
+        ))}
+      </NativeSelect>
+    </FormControl>
+  );
+};
 
-export default StatePicker
+export default StatePicker;
--- src/api/index.js
+++ src/api/index.js
@@ -1,48 +1,45 @@
 import axios from 'axios';
 
-const url = 'https://covid19.mathdro.id/api'
+const url = 'https://covid19.mathdro.id/api';
 
-export const fetchData =async(country) =>{
-    let changableUrl = url;
+export const fetchData = async (country) => {
+  let changableUrl = url;
 
-    if(country &&  country !== 'Global'){
-        changableUrl = `${url}/countries/${country}`
-    }
+  if (country && country !== 'Global') {
+    changableUrl = `${url}/countries/${country}`;
+  }
 
-    try{
-
-        const {data : {confirmed,recovered,deaths,lastUpdate}} =  await axios.get(changableUrl);
-      
-
-        return {confirmed,recovered,deaths,lastUpdate};
-    }catch(error){
-        return error;
-    }
-}
+  try {
+    const {
+      data: { confirmed, recovered, deaths, lastUpdate },
+    } = await axios.get(changableUrl);
+    return { confirmed, recovered, deaths, lastUpdate };
+  } catch (error) {
+    return error;
+  }
+};
 
 export const fetchDailyData = async () => {
-    try {
-        const {data} = await axios.get(`${url}/daily`);
-        
-        return data.map((DailyData) =>({
-            confirmed: DailyData.confirmed.total,
-            deaths: DailyData.deaths.total,
-            date: DailyData.reportDate,
-        }))
-    }
-    catch(error){
-        console.log(error)
-    }
-}
+  try {
+    const { data } = await axios.get(`${url}/daily`);
+
+    return data.map((DailyData) => ({
+      confirmed: DailyData.confirmed.total,
+      deaths: DailyData.deaths.total,
+      date: DailyData.reportDate,
+    }));
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export const fetchStates = async () => {
-    try {
-        const {data:{countries}} = await axios.get(`${url}/countries`);
-        return countries.map((country)=>country.name)
-      
-        
-    }
-    catch(error){
-        console.log(error)
-    }
-}
+  try {
+    const {
+      data: { countries },
+    } = await axios.get(`${url}/countries`);
+    return countries.map((country) => country.name);
+  } catch (error) {
+    console.log(error);
+  }
+};
